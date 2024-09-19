ef55aaa66296ab7b83c70eaa99e32add356416ef
kelmith <faye.12222883@lpu.in>
2024-09-19 13:07:34 +0530

    draft one

---
+++ lib/data.ts
@@ -0,0 +1,121 @@
+export const data = [
+  {
+    id: 1,
+    name: "Arnold Schwarzenegger",
+    role: "Actor",
+    company: "Terminator",
+    information:
+      "Arnold Schwarzenegger is an Austrian-American actor, producer, businessman, retired bodybuilder, and former politician who served as the 38th governor of California from 2003 to 2011.",
+    image:
+      "https://previews.dropbox.com/p/thumb/ACa7dIA0NMCinhZO5JoMmLo71FYyUaupUiYL5nl9eC3yhZVRux6Sb1JBCQbfpXXLNZxhFyb3qX9wlhahWaHTkHJlYpyAC8IcMHdGxYK63dCohiaYi3CFpn2WcuMU0vQfa_xcQZgPeugZRB_Uk9hCfLG0V6qksHJDeamLE-Qsm2k5_hyVqIlC6bDUYgnzPUG7EbyTbDO2GgkHoiGi-DUXX0DIpwTsnGg2fQdXrBwx5HM5_XXPWE6oOX4pK01-KVW6POneqkwpBNUITtUeYANhWn9SgTl8awPPLfE4jGX8n1fDIxpZ6oqXGmNJEwfi4K3fVAGbKih_CYu7fF3TYQ-Td8IcEXYHEm3TB5nQG0v6HyIHh_l9C96bJMqLlJwGHpveZHB_U8vOuK5JG10cONHuAHIE/p.jpeg?is_prewarmed=true",
+  },
+  {
+    id: 2,
+    name: "Francis Ngannou",
+    role: "MMA Fighter",
+    company: "UFC",
+    information:
+      "Francis Ngannou is a Cameroonian-French professional mixed martial artist. He currently competes in the Heavyweight division for the Ultimate Fighting Championship (UFC).",
+    image:
+      "https://previews.dropbox.com/p/thumb/ACbn4yj1AKmDk-q2uvcxvTR03A9iswkOZHaWkKSceCr7vIaPv64qW4aB4sVXpeEo6hHdHC1nk7Pk3aXMOV2B5hFPMtGQvCOvI_UfRSUmAyd3_yLGPPvRJBc6-es5_ku2G1VgarjFQ-S8C7eEroZc_KPKnACY5RNAWHBlXebrvhZjT5DwUOsr8EGIXM-fcIsJBRsCbeAaZPYGbqtsU6y7c3uW3PlcRSbJ06FTwPgoBAZ6d0hnP-K_W4dl1eEquZgw7OhHqC6TSWdchONFxn1oAuaaYflryrUPEQwd8msFth7yUit1dkVS0v7BQwgteq9ltDe0bbt1GI9L5Grn-SJjw9PG/p.jpeg?is_prewarmed=true",
+  },
+  {
+    id: 3,
+    name: "Elon Musk",
+    role: "CEO",
+    company: "Tesla",
+    information:
+      "Elon Reeve Musk FRS is a business magnate, industrial designer, and engineer. He is the founder, CEO, CTO, and chief designer of SpaceX; early investor, CEO, and product architect of Tesla, Inc.",
+    image:
+      "https://previews.dropbox.com/p/thumb/ACYDgj6R4saKS6IQpKrFSsgieCPCfkvSxLrKngON9LCvdhOC0rwFAVrftF0GTvsA_VlvIYpd-levatE5aAyl7g9BhqBa9VwxZ8WkEXppZVb6GRTxAT8hjXKr7yBXdRe-0MFG996m4nWsk6a18CdHC8tMrmdjEEI2I-49psO1SYjjFXdB9kQO0N2nrQQr7rmEBcctrFMtqZou_K_srCGYHPh_WifsPP1OJMw69gamQnwT05Oz0sLfIT_qcY4f2Wittpw2e0Y_JeVhi5JYpy6T2MyoZUftQFMy22innaG2HhJBB6mzm5X_jW9UYx6j2kwmN6UvA58TDshwcIT6Sj3Znsif/p.jpeg?is_prewarmed=true",
+  },
+  {
+    id: 4,
+    name: "Lee Kuan Yew",
+    role: "Prime Minister",
+    company: "Singapore",
+    information:
+      "Lee Kuan Yew was a Singaporean politician who was the founding Prime Minister of Singapore, governing for three decades. He is recognised as the nation's founding father, with the country described as transitioning from the third world to first world in a single generation under his leadership.",
+    image:
+      "https://previews.dropbox.com/p/thumb/ACZmmxZjpMnaSkFOQtNzq4geI-Ot2kMJKOl-55Z_gs1roU89W2-1WBLs4OnIMNW_k6Z2S8rN6wIoA_YOpRfHsij6zPjA2L35pEsUMCfhmcyf_jeWLF8LcWT9gmZNZJRg0lMw30wb1p-OoX68uphdERZI7WKUH36eQj7G5M4YOTjY2ZZShBLE4pOUls7IXCtL3wAtSnwkZ5hNdCUvsVyJugUU7Jxh2kpBTsB00Vcrg7ZAKlNsN849lqV9jR1rqnJlVNO5FW-sK8smoT0HZnmT252Tmq5At_yu775r62k5vQkEshWsbet_exBtspWaWe9cuaNTSNTC9pRLMs4D4s0G5BW9/p.png?is_prewarmed=true",
+  },
+  {
+    id: 5,
+    name: "Patrick Bet David",
+    role: "CEO",
+    company: "PHP Agency",
+    information:
+      "Patrick Bet-David is an American entrepreneur and financial adviser. In 2009, Bet-David founded PHP Agency, a financial services marketing organization, which offers a broad array of life insurance products including 401k roll-overs, annuities and health insurance.",
+    image:
+      "https://previews.dropbox.com/p/thumb/ACZmmxZjpMnaSkFOQtNzq4geI-Ot2kMJKOl-55Z_gs1roU89W2-1WBLs4OnIMNW_k6Z2S8rN6wIoA_YOpRfHsij6zPjA2L35pEsUMCfhmcyf_jeWLF8LcWT9gmZNZJRg0lMw30wb1p-OoX68uphdERZI7WKUH36eQj7G5M4YOTjY2ZZShBLE4pOUls7IXCtL3wAtSnwkZ5hNdCUvsVyJugUU7Jxh2kpBTsB00Vcrg7ZAKlNsN849lqV9jR1rqnJlVNO5FW-sK8smoT0HZnmT252Tmq5At_yu775r62k5vQkEshWsbet_exBtspWaWe9cuaNTSNTC9pRLMs4D4s0G5BW9/p.png?is_prewarmed=true",
+  },
+  {
+    id: 6,
+    name: "Jordan B. Peterson",
+    role: "Psychologist",
+    company: "University of Toronto",
+    information:
+      "Jordan Bernt Peterson is a Canadian professor of psychology, clinical psychologist, YouTube personality, and author. He is a professor of psychology at the University of Toronto.",
+    image:
+      "https://previews.dropbox.com/p/thumb/ACYKIVr8UCJU9ULbfcNzonEHEVdQQqdmZhYwXUgh9ByEjHdCD6tNyRI1CfdJ9TAyvD7KmrFzCPrsTjpXNj8vkAupzH_SjxhEAGlD1T34Ec6gtP0od4mJ-xmMGPROoG5aXcU7sgHuj0vYeki9pfX7nvqe_L1m6Mf-fv_a7K8J2xlQn1E5GYuDjB9vqqHaDLlM6Ea4IYA0mN1MKYB1flmfsTG8kOfGufJe8cxxWfwGUkjNUjvC9jh68KIau8xQ2p-IjMnae8vEEs15jl4Zj6tZ0hOgJex04s3B_39BK-Q0y2H1S7QrJz77RqFaZr7M3SegKakx0-pALyoE9fBdyaAVPv0M/p.jpeg?is_prewarmed=true",
+  },
+  {
+    id: 7,
+    name: "Hellen Keller",
+    role: "Author",
+    company: "The Story of My Life",
+    information:
+      "Hellen Adams Keller was an American author, political activist, and lecturer. She was the first deaf-blind person to earn a Bachelor of Arts degree.",
+    image:
+      "https://previews.dropbox.com/p/thumb/ACY0L6mcee5f28nZ3cj3P7y9-ih7SGecvfsAdZ6_fPy1N7gTotQK68MkkeC4fiozELQyze8bompOccgVELvw4-jRhlZ2W5CGkVQDpZWhI6lKV5E5dYRYwts6ZvkdVrn-HRWxHSrIBa6tkEXdEkejL7YyA-yc-Crdj-ZyCxdPytZoZL7nFwU_E3ysCBcJGm0pQFjwTVbEI3CzCoMBwi-AKiFhGXIl9CcUjwzs2XBdiOPt5ywg1OMklFvGiPVTe_T9ZpIjL3atOuiBo4dGRvV3KYejVHHX5BFD3TqqYZO2O3sc-BcVPaTqVZFJ4kxUfwVlZBuy9Je1Nma_faknGug6FFl7/p.jpeg?is_prewarmed=true",
+  },
+  {
+    id: 8,
+    name: "Kobe Bryant",
+    role: "Basketball Player",
+    company: "Los Angeles Lakers",
+    information:
+      "Kobe Bean Bryant was an American professional basketball player. A shooting guard, he spent his entire 20-year career with the Los Angeles Lakers in the National Basketball Association (NBA).",
+    image:
+      "https://previews.dropbox.com/p/thumb/ACaxi6nlvKo-rdSgeYjXa6cTxHYFH7I22r7lnOx0DoV-Q6rLADqSgp9-Ud7qi57PjF8xbhkhVXQS7yz_dzKE5q8e-fu6vyvBPgQdLwah1f9fDMKR9sruGgEXiIBfL5wcpQSZKIthE7V47o_eozCiPqmHm0_CkY3q3n81ztdEfZuj1W7dA88Uq6shDdgaAy0nqioUATF-MEUX-uz_RheAiwHMNbAvbC40YVPlbxSFf0tQA420iqp6wXxgHjzdO-IAuyG0UETvJf-xdshoOg0FXLmV6Dg_dzlwUr-Srs-rMX_9wctwYTczGGxpG9rqpBYryTwkdgmDWqFu2fIoP9HSZtyP/p.jpeg?is_prewarmed=true",
+  },
+  {
+    id: 9,
+    name: "Muniba Mazari",
+    role: "Artist",
+    company: "United Nations",
+    information:
+      "Muniba Mazari is a Pakistani artist, model, activist, motivational speaker, singer, and television host. She uses a wheelchair due to injuries sustained in a car accident at the age of 21 which makes her Pakistan's first wheelchair-using model.",
+    image:
+      "https://previews.dropbox.com/p/thumb/ACbeITDoNUFK0TLr1eaVT2c5T361pZU0LVkP6NImeSr0bYweDdWOX2ZEO3cjegktCEpq7R-h7ZLdoPFUGxg7c3i3HeHd1aMRhSaSbqwHhHXmJuCCDrCC1lzSIA3ngpZlngkAaaZnHpjofLO8MYiy7uHdh9ar9KskpBKThQyzg490pCQhgMZv1FsbA43CO2o9Slsll9awJaGPtNMLKpnlvwQtMLCEGdMj7gPsy3NXO10iJxi5uLk4Ahz831Ywyf_S95FnzUTCwmOo48Dvhg1ROq6ZMk3AXojRFKffusbzxKSeNpvR4VrPHlCN6yD-6_GqOHJyzc107gasqfnYaY1X2DOY/p.jpeg?is_prewarmed=true",
+  },
+  {
+    id: 10,
+    name: "Vusi Thembekwayo",
+    role: "Entrepreneur",
+    company: "My Growth Fund",
+    information:
+      "Vusi Thembekwayo is a South African venture capitalist, entrepreneur, and public speaker. He is the CEO of MyGrowthFund, a venture capital company that invests in small and medium-sized businesses.",
+    image:
+      "https://previews.dropbox.com/p/thumb/ACZOqnp1LTspayK0XKzL7mgIIL-YS8VxfAAtQXO8BcERPoIrPF8-UH3INzVhiQhIAhAObvXTVTjzCeEkDjlO5z5U9EkF7VccHG626uevBK7X85gp-q8N8OmEB7u-PNQ3H5vUJicHX6ub-kspE4mTQwPpduU-qIOoyUKZ_f96TN5cQTAE8ZyQQIawLZS7e7A2pybkJD8UwUjWxe0T_I5boLfo299yWmv55c8Gxe0FqVxNJAp4nA4bIU1ToNvY7OiG21pMpU2wwVFx9rNVIL01eg2dq70vKpQcMXA93Co5wVPCWh9d-A5noU2rZGXfj1vPm1ULcAARP7Oe1s459mSLYa3t/p.jpeg?is_prewarmed=true",
+  },
+  {
+    id: 11,
+    name: "Jamie Dimon",
+    role: "CEO",
+    company: "JPMorgan Chase",
+    information:
+      "Jamie Dimon is an American business executive. He is chairman and CEO of JPMorgan Chase, the largest of the big four American banks, and was previously on the board of directors of the Federal Reserve Bank of New York.",
+    image:
+      "https://previews.dropbox.com/p/thumb/ACZjQGnhItIM5tgbqoVV9kmxJY5EVF_-Eyr-8yGBuS6M6eOVVtSCuaCTBzgIOyVyhFXlcD7Pq31TwLHcxWezFMMU735QG2jvkw8-dogotF_DjuxNEHzI6SCwZgGPyIDIrj-7xAAa8vA_mYUDXKsCLC7grnbNfXfQanjOS9Eh2eCb3aTCqV7sKyb15LRNP6CvqRDdH6BOchZnTdjc1JdX0gaT5cctcPsxov7_iw5mZlgz862uhgA-RA5YG8aeDkWmQdhlFJcZvvOCuumGXbKOeK2z9KJ3-BxkL6y8413qwE3aD_liUrPwh2FuXTJKjJMxzNRDxFccOg1xlYFxoASBuzhJ/p.jpeg?is_prewarmed=true",
+  },
+  {
+    id: 12,
+    name: "Faye Kelmith",
+    role: "CEO",
+    company: "Vybe Business Group",
+    information: "Faye Kelmith is writing a mythical legacy...",
+    image:
+      "https://previews.dropbox.com/p/thumb/ACYsUG6_B7g4zPnA263CvPJg7B_epwCyBDtKjO2W6XapCcna2ETeHDB-OPZZYHEY5IDMWgPQwWB6-0iEQZZDm6-gscnk3w9wq3niufeD12OlgcOiQrp5jcSDaWfZtciE6yQFa7P1_tF6Le5RezwMq9QLqwgiaZFqMRPcZi9QxkNSYA3YgQTZOiFoL5C5ZD-EwPcYU96mch7IpdLqymo8c6_VUZwtyJ84iCuz-FulTyALw32PKXW1qjT-TruXYsmUpIagSX9lqKvH02zPnnmChjSD8vGdmHyk9Y0MsH-qvsQgrPMAEWIPiz3iYLGEzQidEGcQpJuzdmP5RHC_kARoP2yF/p.png?is_prewarmed=true",
+  },
+];
